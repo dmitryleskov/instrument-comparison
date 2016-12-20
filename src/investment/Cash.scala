bc6e3a84a7beeee71220710aa32e688605544b5c
Dmitry Leskov <git@dmitryleskov.com>
2016-12-20 20:37:49 +0700

    Add instruments: ALRS, NVTK, and ruble cash

--- src/investment/Cash.scala
+++ src/investment/Cash.scala
@@ -2,6 +2,19 @@ package investment
 
 import java.time.YearMonth
 
+case object CashRUB extends Instrument {
+  override def toString = "RUB"
+
+  override val startDate = USDRUB.startDate
+  override val endDate = USDRUB.endDate
+
+  /** The price in rubles of purchasing one unit of the given instrument in the given month. */
+  override def price(ym: YearMonth): Double = 1.0
+
+  /** Yield (interest, dividends, etc) of the given instrument in the given month in percents to the price */
+  override def yieldPercentage(ym: YearMonth): Double = 0.0
+}
+
 case object CashUSD extends Instrument {
   override def toString = "USD"
 
