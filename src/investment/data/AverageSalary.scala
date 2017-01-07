6dedbd010b7989f36cb7c3e79159ae78407d3303
Dmitry Leskov <git@dmitryleskov.com>
2017-01-08 00:30:46 +0700

    Primitively extrapolate average salary data to fill the trailing gap

--- src/investment/data/AverageSalary.scala
+++ src/investment/data/AverageSalary.scala
@@ -1,7 +1,9 @@
 package investment.data
 
 import java.time.YearMonth
+import java.time.temporal.ChronoUnit.MONTHS
 import java.util.Locale
+
 import investment.instruments.Instrument
 import investment.util.CSVFile
 
@@ -16,8 +18,8 @@ case object AverageSalary extends Instrument {
   /** Yield (interest, dividends, etc) of the given instrument in the given month in percents to its value */
   override def yieldPercentage(ym: YearMonth): Double = 0.0
 
-  override lazy val startDate = rates.keys.min
-  override lazy val endDate = rates.keys.max
+  override lazy val startDate: YearMonth = rates.keys.min
+  override lazy val endDate: YearMonth = rates.keys.max
 
   val rates: Map[YearMonth, Double] = {
     val dateFormat = java.time.format.DateTimeFormatter.ofPattern("MMM-yyyy", Locale.US)
@@ -28,6 +30,15 @@ case object AverageSalary extends Instrument {
       val rate = values(1).toDouble
       data(yearmonth) = rate
     }
-    Map() ++ data
+    // The official data lags by about three months. For now, let's assume that the data for
+    // the last few missing months match the last known one, though it may make sense to analyze
+    // the last 12 known months to adjust for the December peak.
+    // TODO: Extrapolate better.
+    val lastKnownMonth = data.keys.max
+    val lastKnownValue = data(lastKnownMonth)
+    (1 until lastKnownMonth.until(YearMonth.now(), MONTHS).toInt)
+      .map { lastKnownMonth.plusMonths(_) }
+      .map { (ym) => (ym, lastKnownValue) }
+      .toMap ++ data
   }
 }
